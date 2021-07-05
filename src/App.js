5b8a7e1603dfe092ad1bf425bb187a1fb2e83a57
kpeinado <kpeinado@everis.com>
2021-07-05 08:27:38 -0500

    creacion de LoginPage

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./components/shared/Header";
 import SiderMenu from "./components/shared/SiderMenu";
 import Footer from "./components/shared/Footer";
-import Peticiones from "./pages/Peticiones";
+
 import { Layout } from "antd";
+
+// Import Pages
+import Peticiones from "./pages/Peticiones";
+import LoginPage from "./pages/Login/LoginPage";
+import CasosPruebasScreen from "./pages/CasosDePrueba/CasosPruebasScreen";
 
 const { Content } = Layout;
 
 function App() {
+
+  // Cambiar por un valor false, para visualizar la screen del login
+  const token = true;
+
+  if ( !token ) { return <LoginPage /> }
+
   return (
     <div className="App">
       <Layout>
@@ -23,6 +34,7 @@ function App() {
               >
                 <Route path="/" exact />
                 <Route path="/Peticiones" exact component={Peticiones} />
+                <Route path="/Peticiones/creacion-de-casos-de-prueba" exact component={CasosPruebasScreen} />
               </div>
             </Content>
             <Footer />
---
+++ src/pages/CasosDePrueba/CasosPruebasScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CasosPruebasScreen = () => {
+    
+    return (
+        <h1>CasosPruebasScreen ..!!</h1>
+    );
+}
+
+export default CasosPruebasScreen;
