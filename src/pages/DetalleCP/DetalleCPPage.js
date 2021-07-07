bdd26289794238aa01dd396c4a582b8b2011af19
dlazarte <dlazarte@everis.com>
2021-07-06 23:52:08 -0500

    CKEditor 5

--- src/pages/DetalleCP/DetalleCPPage.js
+++ src/pages/DetalleCP/DetalleCPPage.js
@@ -1,80 +1,77 @@
 import React from "react";
 
-import './detalle-style.css';
+import "./detalle-style.css";
 
-import { 
-    Typography, 
-    Button, 
-    Row, 
-    Col,
-    PageHeader,
-    Space} from 'antd';
-
+import { Typography, Button, Row, Col, PageHeader, Space } from "antd";
+import { InfoCircleTwoTone } from "@ant-design/icons";
 import { SaveFilled } from "@ant-design/icons";
-import info_icon from "../../assets/icons/info.png"
+import info_icon from "../../assets/icons/info.png";
 
 import FormDetalle from "./components/FormDetalle";
 import TableDetallesCP from "./components/TableDetallesCP";
 
 const DetalleCPPage = () => {
+  const { Title } = Typography;
 
-    const { Title } = Typography;
+  return (
+    <>
+      {/** Column para el titulo y el botón general */}
+      <Row>
+        <Col span={24}>
+          <PageHeader
+            onBack={() => window.history.back()}
+            title="Detalle De Casos De Prueba"
+            extra={[
+              <Button
+                shape="round"
+                key="1"
+                icon={<SaveFilled />}
+                type="primary"
+                onClick={() => alert("Botón en mantenimiento ..!!")}
+              >
+                Guardar
+              </Button>,
+            ]}
+          >
+            <div className="card-information">
+              <Space align="center">
+                <InfoCircleTwoTone
+                  className="info_icon"
+                  style={{ fontSize: "40px" }}
+                />
+                {/* <img alt="icon_info" src={info_icon} className="info_icon" /> */}
 
-    return (
-        <>
+                <Title level={5}>
+                  CP - 001 Crear Nueva Ventana Configuración.
+                </Title>
+              </Space>
+              <br />
+              <span className="description-info">
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that it has a...
+              </span>
+            </div>
+          </PageHeader>
+        </Col>
+      </Row>
 
-            {/** Column para el titulo y el botón general */}
-            <Row>
-                <Col span={24}>
-                    <PageHeader
-                        onBack={() => window.history.back()}
-                        title="Detalle De Casos De Prueba"
-                        extra={[
-                            <Button
-                                shape="round"
-                                key="1"
-                                icon={ <SaveFilled/> }
-                                type="primary"
-                                onClick={() => alert("Botón en mantenimiento ..!!")}>
-                                Guardar
-                            </Button>
-                        ]}>
-                            <div className="card-information">
-                                <Space align="center">
-                                    <img alt="icon_info" src={info_icon} className="info_icon" />
-                                
-                                    <Title level={5} >
-                                        CP - 001 Crear Nueva Ventana Configuración.
-                                    </Title>
-                                    
-                                </Space>
-                                <br />
-                                <span className="description-info">
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-                                </span>
-                                
-                            </div>
-                    </PageHeader>
-                </Col>
-            </Row>
+      {/** Column para pasos (steps) */}
+      <Row className="steps-pasos">
+        <Col span={24} className="setps">
+          <FormDetalle />
+        </Col>
+      </Row>
 
-            {/** Column para pasos (steps) */}
-            <Row className="steps-pasos">
-                <Col span={24} className="setps">
-                    <FormDetalle />
-                </Col>
-            </Row>
-
-            {/** Column para la tabla */}
-            <Row className="table-detalleCp">
-                <Col span={24}>
-                    <Title level={4}>Listado De Todos Los Pasos</Title>
-                    <TableDetallesCP />
-                </Col>
-            </Row>
-            
-        </>
-    );
+      {/** Column para la tabla */}
+      <Row className="table-detalleCp">
+        <Col span={24}>
+          <Title level={4}>Listado De Todos Los Pasos</Title>
+          <TableDetallesCP />
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default DetalleCPPage;
--- src/pages/DetalleCP/components/FormDetalle.js
+++ src/pages/DetalleCP/components/FormDetalle.js
@@ -1,19 +1,85 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import { Steps } from 'antd';
+import "../detalle-style.css";
+import { Steps, Button, message } from "antd";
+import EditorStepDetalle from "./EditorStepDetalle";
 
 const FormDetalle = () => {
+  const { Step } = Steps;
+  const [editorData, setEditorData] = useState(
+    "<ul><li>Precondición del caso de prueba!</li><li>Segunda precondición.</li><li>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</li></ul>"
+  );
+  const [current, setCurrent] = React.useState(0);
+  console.log(editorData);
 
-    const { Step } = Steps;
-    
-    return(
-        <Steps current={1} percent={60}>
-            <Step title="Precondición" />
-            <Step title="Acci´ón" />
-            <Step title="Resultado Esperado" />
-            <Step title="Evidencias" />
-        </Steps>
-    );
-}
+  const steps = [
+    {
+      title: "Precondición",
+      content: (
+        <EditorStepDetalle
+          editorData={editorData}
+          setEditorData={setEditorData}
+        />
+      ),
+    },
+    {
+      title: "Acción",
+      content: "Acción",
+    },
+    {
+      title: "Resultado Esperado",
+      content: "Resultado Esperado",
+    },
+    {
+      title: "Evidencias",
+      content: "Evidencias",
+    },
+  ];
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+
+  return (
+    /*  <Steps current={1} percent={60}>
+      <Step title="Precondición" />
+      <Step title="Acción" />
+      <Step title="Resultado Esperado" />
+      <Step title="Evidencias" />
+    </Steps> */
+    <>
+      <Steps current={current}>
+        {steps.map((item) => (
+          <Step key={item.title} title={item.title} />
+        ))}
+      </Steps>
+      <div className="steps-content">{steps[current].content}</div>
+      <div className="steps-action">
+        {current < steps.length - 1 && (
+          <Button type="primary" onClick={() => next()}>
+            Siguiente
+          </Button>
+        )}
+        {current === steps.length - 1 && (
+          <Button
+            type="primary"
+            onClick={() => message.success("Processing complete!")}
+          >
+            Completar
+          </Button>
+        )}
+        {current > 0 && (
+          <Button style={{ margin: "0 8px" }} onClick={() => prev()}>
+            Anterior
+          </Button>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default FormDetalle;
