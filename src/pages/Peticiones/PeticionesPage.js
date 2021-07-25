886df52c003e01af1ecbb0d68c358e0a4470948c
dlazarte <dlazarte@everis.com>
2021-07-25 03:09:46 -0500

    Test ancho dinámico tabla peticiones

--- src/pages/Peticiones/PeticionesPage.js
+++ src/pages/Peticiones/PeticionesPage.js
@@ -38,6 +38,41 @@ export default function Peticionespage() {
     );
     setListPeticiones(results);
   }, [searchTerm]); */
+  console.log(
+    window.screen.width > 1280
+      ? "mayor a 1280"
+      : window.screen.width > 1920
+      ? "mayor a 1920"
+      : "pequeña"
+  );
+
+  const getSpan = () => {
+    const width = window.screen.width;
+    if (width >= 1920) {
+      console.log(12);
+      return 12;
+    } else if (width >= 1280) {
+      console.log(16);
+      return 16;
+    } else {
+      console.log(24);
+      return 24;
+    }
+  };
+
+  const getOffset = () => {
+    const width = window.screen.width;
+    if (width >= 1920) {
+      console.log(6);
+      return 6;
+    } else if (width >= 1280) {
+      console.log(4);
+      return 4;
+    } else {
+      console.log(0);
+      return 0;
+    }
+  };
 
   return (
     <>
@@ -55,16 +90,13 @@ export default function Peticionespage() {
         </Row>
 
         <Row style={{ marginTop: 20 }}>
-          <Col span={12} offset={6}>
+          <Col span={getSpan()} offset={getOffset()}>
             <ListPeticiones
               listData={listPeticiones}
               showModal={showModal}
               setShowModal={setShowModal}
             />
           </Col>
-          {/* <Col span={10} offset={1}>
-            <InfoPeticion peticion={detallePeticion} />
-          </Col> */}
         </Row>
         <br />
 
