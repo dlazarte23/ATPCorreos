69b7fa1ff990b7aaa2fbf9118d1f59a1278bcbb5
dlazarte <dlazarte@everis.com>
2021-07-26 00:20:57 -0500

    Custom basic editor

--- src/pages/DetalleCP/components/EditorStepDetalle.js
+++ src/pages/DetalleCP/components/EditorStepDetalle.js
@@ -2,6 +2,20 @@ import React from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
+const editorConfiguration = {
+  toolbar: [
+    "bold",
+    "italic",
+    "underLine",
+    "|",
+    "bulletedList",
+    "numberedList",
+    "|",
+    "undo",
+    "redo",
+  ],
+};
+
 const EditorStepDetalle = (props) => {
   const { stepData, setStepData, initialData, current } = props;
 
@@ -33,11 +47,13 @@ const EditorStepDetalle = (props) => {
     <CKEditor
       editor={ClassicEditor}
       data={initialData}
+      config={editorConfiguration}
       onReady={(editor) => {
         //console.log("Editor is ready to use!", editor);
       }}
       onChange={(event, editor) => {
         handleChangeContent(editor.getData());
+        console.log(editor.getData());
       }}
       onBlur={(event, editor) => {
         //console.log("Blur.", editor);
