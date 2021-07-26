2ad9c8f4aee847101439a7b4c7d402cfd91b8bca
Mariangel Mora Marquez <mmoramar@everis.com>
2021-07-26 17:52:44 -0500

    [insert] field in modal edit, create and drawer peticiones

--- src/pages/Login/components/FormLogin.js
+++ src/pages/Login/components/FormLogin.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { Form, Input, Button, Typography, Card } from "antd";
 import { RightOutlined } from "@ant-design/icons";
@@ -7,37 +7,37 @@ import { useHistory } from "react-router-dom";
 
 import { useDispatch } from "react-redux";
 
-import { logearUsuario } from "../../../stateManagement/actions/usuarioAction";
+import { logearUsuario } from '../../../stateManagement/actions/usuarioAction';
 
 const FormLogin = () => {
+
   const { Title } = Typography;
 
-  const history = useHistory();
-  const [isLogging, setIsLogging] = useState(false);
+  const history = useHistory( );
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch( );
 
-  const loginUsuario = (usuario) => dispatch(logearUsuario(usuario));
+  const loginUsuario = usuario => dispatch( logearUsuario( usuario ) );
 
-  const onFinish = async (values) => {
-    setIsLogging(true);
-    const response = await loginUsuario(values);
+  const onFinish = async ( values ) => {
 
-    if (response === "200") {
-      localStorage.setItem("IS_AUTHENTICATED", true);
-      //Redirigiendo al home...
-      history.push("/peticiones");
+    const response = await loginUsuario( values );
+
+    if ( response === "200" ) {
+      history.push('/peticiones');
     }
+
   };
 
   const onFinishFailed = (errorInfo) => {
-    setIsLogging(false);
     console.log("Failed:", errorInfo);
   };
 
   return (
     <div className="contenedor-formLogin">
+
       <Card>
+
         <Title level={3} className="tituloIniciarSesion">
           Iniciar Sesión
         </Title>
@@ -49,42 +49,45 @@ const FormLogin = () => {
           layout="vertical"
           initialValues={{ remember: true }}
           onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          hideRequiredMark
-        >
+          onFinishFailed={onFinishFailed} 
+          hideRequiredMark>
+
           <Form.Item
             label="Usuario o Email"
             name="username"
-            rules={[
-              { required: true, message: "Debe ingresar su usuario o email!" },
-            ]}
-          >
+            rules={[{ required: true, message: "Debe ingresar su usuario o email!" }]} >
+
             <Input />
+
           </Form.Item>
 
           <Form.Item
             label="Contraseña"
             name="password"
-            rules={[
-              { required: true, message: "Debe ingresar su contraseña!" },
-            ]}
-          >
+            rules={[{ required: true, message: "Debe ingresar su contraseña!" }]} >
+
             <Input.Password />
+
           </Form.Item>
 
           <Form.Item className="btnVamos">
+
             <Button
               shape="round"
+             
               type="primary"
-              htmlType="submit"
-              loading={isLogging}
-            >
-              Iniciar Sesión
-              <RightOutlined />
+              htmlType="submit" >
+
+             Iniciar Sesión
+             <RightOutlined />
             </Button>
+
           </Form.Item>
+
         </Form>
+
       </Card>
+
     </div>
   );
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,33 +1,14 @@
-import { Route, Switch, Redirect } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+
 import { auth, main } from "./routes";
 
 const Routing = () => {
-  const isAuthenticated = !!localStorage.getItem("IS_AUTHENTICATED");
-  const isLoggedIn = useSelector((state) => state.usuario.loggedIn);
-  console.log(isAuthenticated);
-  return (
-    <Switch>
-      <Route
-        path={auth.path}
-        //component={auth.component}
-        render={() =>
-          isAuthenticated ? <Redirect to="/peticiones" /> : <auth.component />
-        }
-      />
-      <Route
-        path={main.path}
-        //component={main.component}
-        render={() =>
-          isAuthenticated || isLoggedIn ? (
-            <main.component />
-          ) : (
-            <Redirect to="/auth" />
-          )
-        }
-      />
-    </Switch>
-  );
-};
+    return (
+        <Switch>
+            <Route path={auth.path} component={auth.component} />
+            <Route path={main.path} component={main.component} />
+        </Switch>
+    )
+}
 
 export default Routing;
