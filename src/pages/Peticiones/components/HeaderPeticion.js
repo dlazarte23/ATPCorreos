10a77fe48773f9037c3ec6106aee906c50ac8eb3
jruizsia <jruizsia@everis.com>
2021-08-03 16:44:00 -0500

    modificaciones de nombres

--- src/pages/Peticiones/components/HeaderPeticion.js
+++ src/pages/Peticiones/components/HeaderPeticion.js
@@ -76,7 +76,7 @@ export default function HeaderPeticion(props) {
   return (
     <PageHeader
       className="site-page-header"
-      title="Lista de Peticiones"
+      title="Lista de peticiones"
       style={{ paddingLeft: "50px" }}
       extra={[
         <Button
@@ -87,7 +87,7 @@ export default function HeaderPeticion(props) {
           icon={<PlusOutlined />}
           onClick={() => setShowModal({ ...showModal, create: true })}
         >
-          Nueva Petición
+          Nueva petición
         </Button>,
       ]}
     >
