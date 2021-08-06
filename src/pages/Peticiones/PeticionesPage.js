6b164e516189118086d0c3d07537a95719dfbbee
Mariangel Mora Marquez <mmoramar@everis.com>
2021-08-05 19:30:05 -0500

    quitar elementos que no se usan

--- src/pages/Peticiones/PeticionesPage.js
+++ src/pages/Peticiones/PeticionesPage.js
@@ -44,7 +44,7 @@ export default function Peticionespage() {
       peticion.codPeticion.includes(searchTerm.toLowerCase())
     );
     setFilteredData(results);
-  }, [searchTerm, peticiones]);
+  }, [peticiones, searchTerm]);
 
   const getSpan = () => {
     const width = window.screen.width;
