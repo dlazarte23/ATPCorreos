a04dc6371b4e87a9423ad52e96071580f859cd0d
Mariangel Mora Marquez <mmoramar@everis.com>
2021-08-06 09:52:32 -0500

    delete modal detalle

--- src/pages/Peticiones/components/index.js
+++ src/pages/Peticiones/components/index.js
@@ -1,7 +1,6 @@
 export { default as ListPeticiones } from "./ListPeticion";
 export { default as DetailPeticion } from "./DetailPeticion";
 export { default as HeaderPeticion } from "./HeaderPeticion";
-export { default as ModalDetallePeticion } from "./ModalDetallePeticion";
 export { default as ModalCreatePeticion } from "./ModalCreatePeticion";
 export { default as InfoPeticion } from "./InfoPeticion";
 export { default as ListPlandePrueba } from "./ListPlandePrueba";
