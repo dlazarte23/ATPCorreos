19d2b640221a332234d5d947b873ddd0b397f33b
dlazarte <dlazarte@everis.com>
2021-08-11 11:52:15 -0500

    Cambios en basepath env

--- public/env-config.js
+++ public/env-config.js
@@ -1,4 +1,4 @@
 window._env_ = { 
-  REACT_APP_ATPCORREOS: "http://localhost:8087/s3link/", 
+  REACT_APP_ATPCORREOS: "http://backtestplan.herokuapp.com/s3link/", 
   SKIP_PREFLIGHT_CHECK: "true", 
 } 
