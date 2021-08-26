d492cdd82f4bd08813e7f16e1a37a152e94da575
kpeinado <kpeinado@everis.com>
2021-08-26 18:58:38 -0500

    cambios de la ruta del back

--- public/env-config.js
+++ public/env-config.js
@@ -1,4 +1,4 @@
 window._env_ = { 
-  REACT_APP_ATPCORREOS: "https://backtestplan.herokuapp.com/s3link/", 
+  REACT_APP_ATPCORREOS: "https://ec2-3-89-35-173.compute-1.amazonaws.com/s3link/", 
   SKIP_PREFLIGHT_CHECK: "true", 
 } 
