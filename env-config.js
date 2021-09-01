ef8bdee82edbc763f9649de33b97bf772d7cabe8
kpeinado <kpeinado@everis.com>
2021-09-01 08:12:36 -0500

    nueva url

--- env-config.js
+++ env-config.js
@@ -1,4 +1,4 @@
 window._env_ = { 
-  REACT_APP_ATPCORREOS: "https://ec2-3-95-244-179.compute-1.amazonaws.com/s3link/", 
+  REACT_APP_ATPCORREOS: "https://ec2-3-87-14-239.compute-1.amazonaws.com/s3link/", 
   SKIP_PREFLIGHT_CHECK: "true", 
 } 
