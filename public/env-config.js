cf222967d3cbbcdad1499455a732ea39ab563a4f
dlazarte <dlazarte@everis.com>
2021-12-06 12:40:25 -0500

    Update env

--- public/env-config.js
+++ public/env-config.js
@@ -1,5 +1,4 @@
-window._env_ = {
-  REACT_APP_ATPCORREOS:
-    "https://ec2-54-144-2-48.compute-1.amazonaws.com/s3link",
-  SKIP_PREFLIGHT_CHECK: "true",
-};
+window._env_ = { 
+  REACT_APP_ATPCORREOS: "https://ec2-54-166-42-51.compute-1.amazonaws.com/s3link", 
+  SKIP_PREFLIGHT_CHECK: "true", 
+} 
