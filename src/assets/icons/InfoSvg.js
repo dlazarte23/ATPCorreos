92b781ecfd6a320fe6a2b09b56ab569c80412954
dlazarte <dlazarte@everis.com>
2021-12-06 13:50:05 -0500

    Fix conflicts

--- src/assets/icons/InfoSvg.js
+++ src/assets/icons/InfoSvg.js
@@ -1,3 +1,4 @@
+import React from "react";
 export const InfoSvg = () => (
   <svg
     width="55"
--- src/components/shared/PageNotFound.js
+++ src/components/shared/PageNotFound.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Result, Button } from "antd";
 
 import { ArrowLeftOutlined } from '@ant-design/icons'
