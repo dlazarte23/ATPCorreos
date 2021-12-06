c9e7e49b849b1e03fa476d64fcf5f91462a7dd13
dlazarte <dlazarte@everis.com>
2021-12-06 13:33:09 -0500

    Fix conflicts on deploy

--- src/components/layouts/Breadcrumbs.js
+++ src/components/layouts/Breadcrumbs.js
@@ -1,20 +1,18 @@
+import React from "react";
 import useBreadcrumbs from "use-react-router-breadcrumbs";
 import { breadcrumbRoutes } from "../../routes/routes";
 import { Breadcrumb } from "antd";
 
 const Breadcrumbs = () => {
-
   const breadcrumbs = useBreadcrumbs(breadcrumbRoutes);
 
   return (
     <Breadcrumb>
       {breadcrumbs.map(({ match, breadcrumb }) => (
-        <Breadcrumb.Item key={ match.url }>
-          { breadcrumb }
-        </Breadcrumb.Item>
+        <Breadcrumb.Item key={match.url}>{breadcrumb}</Breadcrumb.Item>
       ))}
     </Breadcrumb>
   );
-}
+};
 
 export default Breadcrumbs;
